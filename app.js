72de3da067a40faa267419ffb99d248003d6d1f7
HugoHenry <quanghieusieubeo@gmail.com>
2019-11-02 07:04:59 +0700

    hieudz

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const path = require("path")
+const handlebars = require("express-handlebars")
+const bodyParser = require("body-parser")
+const fs = require("fs")
+
+let app = express()
+
+let option = {
+    defaultLayout: "main"
+}
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.engine("handlebars", handlebars({defaultLayout: "main"}))
+app.set("view engine","handlebars")
+
+app.get("/",(req,res)=>{
+    res.render("mainpage")
+})
+
+app.post("/Login",(req,res)=>{
+    let UserID = req.body.ID
+    let pass = req.body.Password
+    fs.readFile('data.json','utf-8',(err,data)=>{
+        if(err){console.log(err)}else{
+            let objdata = JSON.parse(data)
+            
+            
+            console.log(objdata)
+            
+        }
+    })
+    console.log(UserID)
+    console.log(pass)
+    res.send("Login Completed")
+    // console.log("Please check your ID again")
+
+
+})
+ 
+app.get("/Login",(req,res)=>{
+    res.render("loginPage")
+})
+
+app.listen(701,(err)=>{
+    if(err){console.log(err)}else{
+        console.log("App listen at 701")
+    }
+})
+
+app.use(express.static("public"))
+
+
